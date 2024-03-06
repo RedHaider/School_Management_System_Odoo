d651f56a2aa0a5d4fbf8bf4a8ab02a3e5027244d
RedHaider <syedaliredwanulhaider13@gmail.com>
2024-03-06 12:31:22 +0600

    first commit

---
+++ SMS_base/models/__init__.py
@@ -0,0 +1,7 @@
+from . import school_student
+from . import school_teacher
+from . import school_parent
+from . import school_class
+from . import school_room
+from . import school_section
+from . import school_subject
